728be3a94776612c63d7ed62541d302c0ff0a9e5
mohanaselvasurjithp <mohanaselvasurjith.p@gmail.com>
2025-11-25 16:09:30 +0530

    Add files via upload

---
+++ VariableAndDatatypes.js
@@ -0,0 +1,23 @@
+//Primitive Data Types in JavaScript
+
+let firstName = "Mohan";
+console.log("My name is : " + firstName);
+console.log(typeof firstName);
+
+let companyName = true;
+console.log(companyName);
+console.log(typeof companyName);        
+
+let mobileNumber = 7708455066;      
+console.log("My mobile number is : " + mobileNumber);
+console.log(typeof mobileNumber);
+
+let hasPlaywright;
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright); 
+
+let isAutomation = null;  
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+
